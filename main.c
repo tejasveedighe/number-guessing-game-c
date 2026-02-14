c8a468f5508a2625e82acef646e01388273f7998
Tejasvee Dighe <tejasveedighe@gmail.com>
2026-02-15 02:56:06 +0530

    v1
    basic game created with core utils

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
-
-#include<stdlib.h> // For rand()
+#include<stdlib.h> // For rand() and system("")
+#include<time.h> // for time()
 
 /*
  * What does a number guessing game do?
@@ -12,15 +12,52 @@
 
 /*
  * Questions:
- * 1. How do random number generate in c?
+ * 1. How do random number generate in c? Ans - rand()
+ * 2. How do i set a limit to the rand() function?
+ * 3. Why do i need to seed the rand() and how?
+ * Ans - because it will seed once during compile and then always return the same value, time(NULL)
+ * 4. Why did scanf work with &input and not just input?
  * */
 
+void clrscr();
+
+void clrscr() {
+  system("clear");
+}
+
 int main() {
+  // clear the terminal
+  clrscr();
+
+  // seeding the rand method
+  srand(time(NULL));
+
+  int minValue = 1, maxValue = 10;
 
   // this function returns a random number
-  int x = rand();
+  int x = minValue + rand() % (maxValue - minValue + 1);
 
-  printf("%d", x);
+  while(true) {
+
+    printf("\nGuess the number, Enter a number: ");
+
+    int input = 0;
+    scanf("%d", &input);
+
+    if(input > x) {
+      printf("\nSmaller");
+    }
+    else if(input < x) {
+      printf("\nLarger");
+    }else if(input == x) {
+      printf("\nYou guessed correctly!!!");
+      break;
+    }
+    int temp = getchar();
+    clrscr();
+  }
+
+  printf("\nGood Bye");
 
   return 0;
 }
